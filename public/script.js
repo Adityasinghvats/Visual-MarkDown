123dd4a4caa102e51ae925a58a10658ec06c0374
AdiX <131326798+Adityasinghvats@users.noreply.github.com>
2025-01-23 14:08:20 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -3,13 +3,36 @@ window.onload = function(){
     const pad = document.getElementById('pad');
     const markdown = document.getElementById('markdown');
 
+    let previousMarkdown;
+
     const convertAreaToMarkDown = () => {
         const markdownText = pad.value;
+        previousMarkdown = markdownText;
         html = convertor.makeHtml(markdownText);
         markdown.innerHTML = html;
     }
 
+    //check if something in pad changes
+    const didChangeOccur = function(){
+        if(previousMarkdown != pad.value){
+            return true;
+        }
+        else return false;
+    };
+
+    //check for change every 1 second
+    setInterval(() => {
+        if(didChangeOccur()){
+            convertAreaToMarkDown();
+        }
+    }, 1000);
+
     pad.addEventListener('input', convertAreaToMarkDown);
 
-    convertAreaToMarkDown();
+    sharejs.open('home', 'text', (err, doc) => {
+        doc.attach_textarea(pad);
+        convertAreaToMarkDown();
+    })
+
+    // convertAreaToMarkDown();
 }
