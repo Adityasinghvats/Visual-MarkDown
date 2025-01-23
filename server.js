326b26c6431de741e271bd3a60dce052e06ecda4
AdiX <131326798+Adityasinghvats@users.noreply.github.com>
2025-01-23 14:09:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import 'dotenv/config';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import sharejs from 'share';
+import 'redis';
+import { type } from 'os';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,6 +20,16 @@ app.use(express.static(__dirname+'/public'));
 app.get('/', (req,res)=>{
     res.render('pad');
 })
+//route with userid
+app.get('/(:id)',(req,res) => {
+    res.render('pad');
+})
+
+const options = {
+    db : {type: 'redis'}
+}
+
+sharejs.server.attach(app, options);
 
 app.listen(port,()=>{
     console.log('Server started')
